cdcb1f4c126b867387c4c85b69342df125bb344e
sonyc2011 <florin.negau@gmail.com>
2026-07-31 20:44:13 +0300

    Update logging to use static TAG for consistency

--- components/tx_ultimate/tx_ultimate_led.cpp
+++ components/tx_ultimate/tx_ultimate_led.cpp
@@ -1,13 +1,17 @@
 #include "tx_ultimate_led.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace tx_ultimate {
 
+static const char *const TAG = "tx_ultimate_led";
+
 void TxUltimateLed::setup() {
-  ESP_LOGI("tx_led", "TX Ultimate LED component started");
+  ESP_LOGI(TAG, "TX Ultimate LED component started");
 }
 
 void TxUltimateLed::loop() {
 }
 
 }  // namespace tx_ultimate
+}  // namespace esphome
