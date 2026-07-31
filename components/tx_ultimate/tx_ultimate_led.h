8b52011505e0d0e13e45e40a1f4780779b1ccfaa
sonyc2011 <florin.negau@gmail.com>
2026-07-31 20:43:12 +0300

    Add TxUltimateLed class with night light functionality

--- components/tx_ultimate/tx_ultimate_led.h
+++ components/tx_ultimate/tx_ultimate_led.h
@@ -5,10 +5,31 @@
 namespace esphome {
 namespace tx_ultimate {
 
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/light/light_state.h"
+
+namespace esphome {
+namespace tx_ultimate {
+
 class TxUltimateLed : public Component {
  public:
   void setup() override;
   void loop() override;
+
+  void enable_night_light();
+  void disable_night_light();
+  void toggle_night_light();
+
+  void set_night_light(light::LightState *light);
+
+ protected:
+  bool night_light_enabled_{false};
+  light::LightState *night_light_{nullptr};
 };
+
+}  // namespace tx_ultimate
+}  // namespace esphome
 
 }  // namespace tx_ultimate
