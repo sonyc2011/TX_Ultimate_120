bba6e588cdc87d331fbcf88b08d62a7307a4c623
sonyc2011 <florin.negau@gmail.com>
2026-07-31 17:04:25 +0300

    Implement TX Ultimate LED component setup and loop

---
+++ components/tx_ultimate/tx_ultimate_led.cpp
@@ -0,0 +1,13 @@
+#include "tx_ultimate_led.h"
+
+namespace esphome {
+namespace tx_ultimate {
+
+void TxUltimateLed::setup() {
+  ESP_LOGI("tx_led", "TX Ultimate LED component started");
+}
+
+void TxUltimateLed::loop() {
+}
+
+}  // namespace tx_ultimate
