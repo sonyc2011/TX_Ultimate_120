67afe164a4e3431e1087608a1bcfca0bb56113c7
sonyc2011 <florin.negau@gmail.com>
2026-07-31 21:03:11 +0300

    Add night light control functions to TxUltimateLed

--- components/tx_ultimate/tx_ultimate_led.cpp
+++ components/tx_ultimate/tx_ultimate_led.cpp
@@ -12,6 +12,22 @@ void TxUltimateLed::setup() {
 
 void TxUltimateLed::loop() {
 }
+void TxUltimateLed::enable_night_light() {
+  night_light_enabled_ = true;
+  ESP_LOGI(TAG, "Night Light ON");
+}
 
+void TxUltimateLed::disable_night_light() {
+  night_light_enabled_ = false;
+  ESP_LOGI(TAG, "Night Light OFF");
+}
+
+void TxUltimateLed::toggle_night_light() {
+  if (night_light_enabled_) {
+    disable_night_light();
+  } else {
+    enable_night_light();
+  }
+}
 }  // namespace tx_ultimate
 }  // namespace esphome
